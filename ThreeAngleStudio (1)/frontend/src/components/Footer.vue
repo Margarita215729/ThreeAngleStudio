71f19d67f1c22915c94bf519c9902cb6fc825e7e
Margarita215729 <107581611+Margarita215729@users.noreply.github.com>
2025-01-17 01:12:51 -0500

    Update Footer.vue

--- ThreeAngleStudio (1)/frontend/src/components/Footer.vue
+++ ThreeAngleStudio (1)/frontend/src/components/Footer.vue
@@ -1,1 +1,21 @@
-<template><footer>Footer Component</footer></template>
+<template>
+  <footer>
+    <p>&copy; 2025 ThreeAngleStudio. All Rights Reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+footer {
+  background-color: #1a1a1a;
+  color: #fffaf1;
+  text-align: center;
+  padding: 1rem;
+  margin-top: 2rem;
+}
+</style>
