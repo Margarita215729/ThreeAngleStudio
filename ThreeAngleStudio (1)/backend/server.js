d7f30af9d5246b3c56b963e7ecbecf06d680b5c1
Margarita215729 <107581611+Margarita215729@users.noreply.github.com>
2025-01-17 01:04:27 -0500

    Update server.js

--- ThreeAngleStudio (1)/backend/server.js
+++ ThreeAngleStudio (1)/backend/server.js
@@ -1,1 +1,21 @@
-// Express server configuration here
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+const mainRoutes = require('./routes/mainRoutes');
+app.use('/api', mainRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
